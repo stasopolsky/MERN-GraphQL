35686d94989a788bcd9ad7f78f8b5df4d39172f6
Stas Opolsky <stas.opolsky@gmail.com>
2025-06-19 14:26:42 +0300

    client mutations and queries

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import Header from "./components/Header";
+import Clients from "./components/Clients";
+import {
+  ApolloCache,
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
+function App() {
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <Clients />
+        </div>
+      </ApolloProvider>
+    </>
+  );
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const colors = require("colors");
+const cors = require("cors");
 const connectDB = require("./config/db");
 const schema = require("./schema/schema");
 require("dotenv").config();
@@ -7,6 +9,7 @@ const { graphqlHTTP } = require("express-graphql");
 const port = 5000;
 connectDB();
 const app = express();
+app.use(cors());
 // console.log(process.env.NODE_ENV);
 app.use(
   "/graphql",
